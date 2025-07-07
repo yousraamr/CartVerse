b1506b4f00bb1069c1dce74dfe59f54f5d58d203
yousraamr <yousraamr000@gmail.com>
2025-07-07 23:42:18 +0200

    Implemented cache_helper.dart for shared preferences operations.

--- lib/utils/cache_helper.dart
+++ lib/utils/cache_helper.dart
@@ -1,5 +1,30 @@
-class CacheRepo
-{
-  
+import 'package:shared_preferences/shared_preferences.dart';
 
+class CacheHelper {
+  static SharedPreferences? _prefs;
+
+  // Initialize
+  static Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  // Save string
+  static Future<bool> saveData({required String key, required String value}) async {
+    return await _prefs!.setString(key, value);
+  }
+
+  // Get string
+  static String? getData({required String key}) {
+    return _prefs!.getString(key);
+  }
+
+  // Remove
+  static Future<bool> removeData({required String key}) async {
+    return await _prefs!.remove(key);
+  }
+
+  // Clear all
+  static Future<bool> clearAll() async {
+    return await _prefs!.clear();
+  }
 }
