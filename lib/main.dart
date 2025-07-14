18ad694d0c1259e0156650031c4b1d6413c49d5f
yousraamr <yousraamr000@gmail.com>
2025-07-14 02:37:57 +0200

    Updated main to support ThemeCubit and apply theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,47 @@
-import 'package:cartverse/utils/custom_router.dart';
-import 'package:cartverse/utils/route_names.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'view/home_page.dart';
+import 'cubit/auth_cubit.dart';
+import 'cubit/theme_cubit.dart';
+import 'services/auth_service.dart';
+import 'utils/themes.dart';
+import 'utils/custom_router.dart';
+import 'utils/route_names.dart';
 import 'utils/cache_helper.dart';
+import 'view/home_page.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper.init();
-  runApp(const MyApp());
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit(AuthService())),
+        BlocProvider(create: (_) => ThemeCubit()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      onGenerateRoute:CustomRouter.allRoutes,
-      initialRoute: homeScreen,
+    return BlocBuilder<ThemeCubit, ThemeMode>(
+      builder: (context, themeMode) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          themeMode: themeMode,
+          home: const HomePage(),
+          onGenerateRoute: CustomRouter.allRoutes,
+          initialRoute: homeScreen,
+        );
+      },
     );
   }
 }
