87822ba9d8dd018f843e85f0a46cd3aaccaf99b3
yousraamr <yousraamr000@gmail.com>
2025-07-14 02:38:09 +0200

    Extended CacheHelper to support bool for theme mode

--- lib/utils/cache_helper.dart
+++ lib/utils/cache_helper.dart
@@ -9,13 +9,23 @@ class CacheHelper {
   }
 
   // Save string
-  static Future<bool> saveData({required String key, required String value}) async {
+  static Future<bool> saveString({required String key, required String value}) async {
     return await _prefs!.setString(key, value);
   }
 
+  // Save boolean
+  static Future<bool> saveBool({required String key, required bool value}) async {
+    return await _prefs!.setBool(key, value);
+  }
+
   // Get string
-  static String? getData({required String key}) {
+  static String? getString({required String key}) {
     return _prefs!.getString(key);
+  }
+
+  // Get boolean
+  static bool? getBool({required String key}) {
+    return _prefs!.getBool(key);
   }
 
   // Remove
