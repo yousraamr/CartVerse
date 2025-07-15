9f0c3a803428f0003e824a80a0b7ec2d2bde35d2
yousraamr <yousraamr000@gmail.com>
2025-07-15 06:14:49 +0200

    Enhance Product model with mapping functions

---
+++ lib/models/product_model.dart
@@ -0,0 +1,39 @@
+class Product {
+  final String id;
+  final String name;
+  final String image;
+  final double price;
+  final String category;
+  final String description;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.category,
+    required this.description,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'image': image,
+      'price': price,
+      'category': category,
+      'description': description,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'],
+      name: map['name'],
+      image: map['image'],
+      price: map['price'],
+      category: map['category'],
+      description: map['description'],
+    );
+  }
+}
