6f63ac9a18ac5dcbf46f224b703e3c87efa8175b
yousraamr <yousraamr000@gmail.com>
2025-07-15 06:14:10 +0200

    Add mock product data

---
+++ lib/data/mock_products.dart
@@ -0,0 +1,36 @@
+import '../models/product_model.dart';
+
+final List<Product> mockProducts = [
+  Product(
+    id: '1',
+    name: 'Men\'s Casual Shirt',
+    image: 'https://static.zara.net/assets/public/eb5c/12e9/cf724809bb9e/c97293d73b26/02878288710-p/02878288710-p.jpg?ts=1749027454017&w=1024',
+    price: 850,
+    category: 'Men',
+    description: 'High-quality cotton shirt perfect for casual wear.',
+  ),
+  Product(
+    id: '2',
+    name: 'Women\'s Elegant Dress',
+    image: 'https://static.zara.net/assets/public/a701/c57d/5f3841b1b7c8/305ec5ac33c5/03644176450-p/03644176450-p.jpg?ts=1742835214699&w=1024',
+    price: 1500,
+    category: 'Women',
+    description: 'Short sleeve midi shirt dress with a v-neck and draped side detail. Metal button fastening at the front.',
+  ),
+  Product(
+    id: '3',
+    name: 'Kids\' Hoodie',
+    image: 'https://static.zara.net/assets/public/11b9/3d2e/715c4dbcb87a/ea36210e3e13/00039679518-p/00039679518-p.jpg?ts=1737108993607&w=1024',
+    price: 750,
+    category: 'Kids',
+    description: 'Warm and cozy hoodie for kids.',
+  ),
+  Product(
+    id: '4',
+    name: 'Sports Tracksuit',
+    image: 'https://assets.adidas.com/images/w_1880,f_auto,q_auto/da6bef6360f04f29b76dc560558b357b_9366/JI8858_21_model.jpg',
+    price: 5500,
+    category: 'Sports',
+    description: 'Comfortable and breathable tracksuit for workouts.',
+  ),
+];
