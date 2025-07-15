31e323714a7b6b895e3e3d5cb29d7e32ce83108d
yousraamr <yousraamr000@gmail.com>
2025-07-15 06:14:42 +0200

    Add CartItem model for cart functionality

---
+++ lib/models/cart_item_model.dart
@@ -0,0 +1,22 @@
+import 'product_model.dart';
+
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem({required this.product, this.quantity = 1});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'product': product.toMap(),
+      'quantity': quantity,
+    };
+  }
+
+  factory CartItem.fromMap(Map<String, dynamic> map) {
+    return CartItem(
+      product: Product.fromMap(Map<String, dynamic>.from(map['product'])),
+      quantity: map['quantity'],
+    );
+  }
+}
