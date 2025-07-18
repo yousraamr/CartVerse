d1465c548e23bcda323d789226c7b155070747c8
yousraamr <yousraamr000@gmail.com>
2025-07-18 16:03:36 +0200

    Added clearCart and getTotal functions

--- lib/cubit/cart_cubit.dart
+++ lib/cubit/cart_cubit.dart
@@ -9,6 +9,7 @@ class CartCubit extends Cubit<List<CartItem>> {
 
   static const String cartKey = 'cart';
 
+  /// Load cart items from local storage
   void loadCart() {
     final data = CacheHelper.getString(key: cartKey);
     if (data != null) {
@@ -17,6 +18,7 @@ class CartCubit extends Cubit<List<CartItem>> {
     }
   }
 
+  /// Add product to cart, or increase its quantity if it exists
   void addToCart(Product product) {
     final updatedList = List<CartItem>.from(state);
     final index = updatedList.indexWhere((item) => item.product.id == product.id);
@@ -31,12 +33,14 @@ class CartCubit extends Cubit<List<CartItem>> {
     _saveCart(updatedList);
   }
 
+  /// Remove product completely from cart
   void removeFromCart(Product product) {
     final updatedList = List<CartItem>.from(state)..removeWhere((item) => item.product.id == product.id);
     emit(updatedList);
     _saveCart(updatedList);
   }
 
+  /// Update quantity of a product
   void updateQuantity(Product product, int quantity) {
     final updatedList = List<CartItem>.from(state);
     final index = updatedList.indexWhere((item) => item.product.id == product.id);
@@ -48,8 +52,20 @@ class CartCubit extends Cubit<List<CartItem>> {
     }
   }
 
+  /// Clear the entire cart
+  void clearCart() {
+    emit([]);
+    _saveCart([]);
+  }
+
+  /// Save cart state to local storage
   void _saveCart(List<CartItem> cart) {
     final jsonString = jsonEncode(cart.map((e) => e.toMap()).toList());
     CacheHelper.saveString(key: cartKey, value: jsonString);
+  }
+
+  /// Calculate total price
+  double getTotal() {
+    return state.fold(0.0, (sum, item) => sum + (item.product.price * item.quantity));
   }
 }
