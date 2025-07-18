e69af012b8409e4402198375e43a061f5fc96642
yousraamr <yousraamr000@gmail.com>
2025-07-18 16:08:51 +0200

    Added localization

--- lib/view/wishlist_page.dart
+++ lib/view/wishlist_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:easy_localization/easy_localization.dart';
 import '../cubit/wishlist_cubit.dart';
 import '../models/product_model.dart';
 import '../view/product_card.dart';
@@ -10,11 +11,11 @@ class WishlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("My Wishlist")),
+      appBar: AppBar(title: Text(tr("wishlist_title"))),
       body: BlocBuilder<WishlistCubit, List<Product>>(
         builder: (context, wishlist) {
           if (wishlist.isEmpty) {
-            return const Center(child: Text("Your wishlist is empty."));
+            return Center(child: Text(tr("wishlist_empty")));
           }
           return GridView.builder(
             padding: const EdgeInsets.all(16),
@@ -31,7 +32,6 @@ class WishlistPage extends StatelessWidget {
             },
           );
         },
-
       ),
     );
   }
